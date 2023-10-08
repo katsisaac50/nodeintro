c709202ca89f309a74b55eca07e5a9a3e9e136ee
katongole <katsisaac50@gmail.com>
2023-10-08 09:20:35 +0200

    add morgan to show status response

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // var superheros = require("superheros")
-var supervillains = require('supervillains');
+// var supervillains = require('supervillains');
 
 // var hero = superheros.random();
 // var villain = supervillains.random();
 
 // console.log(hero);
 
-console.log(supervillains.all);
+// console.log(supervillains.all);
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+app.use(cors());
+ app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.get('/api/users', (req, res) => {
+  res.json({ users: [
+    {name: 'Isaac', age: 21}, 
+    {name: 'Katongole', age: 21}, 
+    {name: 'Katongole', age: 21}, 
+    {name: 'Katongole', age: 21},
+  ], 
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+})
 

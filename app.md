8f451e2e882e11d77edeb9a2ccc8f4eaf59d7856
katongole <katsisaac50@gmail.com>
2023-10-08 11:16:37 +0200

    add mongodb and mongoose

---
+++ app.md
@@ -0,0 +1,3 @@
+mongodb+srv://katsisaac50:<password>@cluster0.nqjxtpf.mongodb.net/?retryWrites=true&w=majority
+
+'mongodb://localhost:27017/hero'
--- index.js
+++ index.js
@@ -13,17 +13,39 @@ const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 const authRouter = require('./routes/auth');
+const mongoose = require('mongoose');
 
+
+// db connection
+mongoose.connect('mongodb+srv://katsisaac50:LovE1234k@cluster0.nqjxtpf.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+
+}).then(() => {
+  console.log('db connected');
+}).catch((err) => {
+  console.log(err);
+});
+
+
+// middlewares
 app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// logging
  app.use(morgan('dev'));
+
+// static files
 app.use(express.static('public'));
 
 // routes middleware
 app.use('/api', authRouter);
 
-
-
-app.listen(5000, () => {
+app.listen(4000, () => {
   console.log('Server is running on port 5000');
 })
 
